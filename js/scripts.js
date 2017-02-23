353b4c3083163a293c9ccac1f5214830b3472ef6
jaden-lesuk <jadenlesuk12@gmail.com>
2017-02-23 16:02:50 +0300

    added logic branching for 3, 5, 15

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,16 @@ $(document).ready(function () {
 		event.preventDefault();
 		var num = parseInt($("#input1").val());
 		var field = [];
+		for (var index = 1; index <= num; index++) {
+			if (index % 15 === 0) {
+				field.push("pingpong")
+			} else if (index % 5 === 0) {
+				field.push("pong")
+			} else if (index % 3 === 0) {
+				field.push("ping")
+			} else {
+				field.push(index)
+			}
+		}
 	});
 });
